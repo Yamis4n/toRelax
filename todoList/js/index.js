a6247f99f85242eaf8f0d7d5a11df89b40eb21b0
Yamis4n <evertonvsf2@gmail.com>
2023-04-29 13:34:07 -0300

    creating new checkbox instead div

--- todoList/js/index.js
+++ todoList/js/index.js
@@ -1,3 +1,5 @@
+let ID = 0;
+
 function createDiv(){
   let taskName = 'novaDiv';  
   let newDiv = document.createElement("div");
@@ -10,7 +12,21 @@ function createDiv(){
   let fixedDiv = document.getElementById("fixedDiv");
   root.insertBefore(newDiv, fixedDiv);
 
+}
 
+function createCheckBox(){
+  let id = "id" + ID++;
+  let labelContent = "nova Atividade"
+  let label = document.createElement("label");
+  let input = document.createElement("input");
+  
+  input.type = "checkbox";
+  input.id = id;
+  
+  label.textContent = labelContent;
+  label.hmtlFor = id;
 
-
+  let root = document.getElementById("root");
+  root.appendChild(input);
+  root.appendChild(label);
 }
