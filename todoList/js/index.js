37604014f88fdbc678c3faa1823110882e781881
Yamis4n <evertonvsf2@gmail.com>
2023-05-11 05:11:48 -0300

    button new task working

--- todoList/js/index.js
+++ todoList/js/index.js
@@ -1,31 +1,31 @@
+const NEWTASK  = document.getElementById("newTaskButton");
+const ROOT     = document.getElementById("tasksZone");
+
 let ID = 0;
 
+
+
 function createDiv(){
-  let taskName = 'Atividade ' + ID;  
-  let div = document.createElement("div");
-  let root = document.getElementById("root");
-  let fixedDiv = document.getElementById("fixedDiv");
+  const div      = document.createElement("div");
+  const text     = document.createTextNode("Task - "+ ID);
   
-  createCheckBox(div, taskName);
-  div.className = "newDiv";
-  
-  root.insertBefore(div, fixedDiv);
+  ID += 1;
+
+  div.appendChild(text);
+  div.className = "new-task";
+  div.id = "task"+ID;
+    
+  ROOT.appendChild(div);
 
 }
 
-function createCheckBox(root, taskName){
-  let id = "id" + ID++;
-  let label = document.createElement("label");
-  let input = document.createElement("input");
-  
-
-  input.type = "checkbox";
-  input.id = id;
-  
-  label.textContent = taskName;
-  label.hmtlFor = id;
-  root.appendChild(input);
-  root.appendChild(label);
+function showDelete(taskTarget) {
+  const taskDiv = document.getElementById(taskTarget);
+  console.log(taskDiv.childNodes.length);
 }
 
-
+ROOT.addEventListener("click", function(event){
+  console.log(event.target);
+  showDelete(event.target.id);
+})
+NEWTASK.addEventListener("click", createDiv);
