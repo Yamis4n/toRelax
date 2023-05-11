6ad902b25803cb818e7015ab3f561375575f96a4
Yamis4n <evertonvsf2@gmail.com>
2023-05-11 05:25:17 -0300

    creating a function to get the task text

--- todoList/js/index.js
+++ todoList/js/index.js
@@ -4,10 +4,10 @@ const ROOT     = document.getElementById("tasksZone");
 let ID = 0;
 
 
-
-function createDiv(){
+// Esta função cria uma div que contém o texto da "tarefa"
+function createDiv(taskContent){
   const div      = document.createElement("div");
-  const text     = document.createTextNode("Task - "+ ID);
+  const text     = document.createTextNode(taskContent);
   
   ID += 1;
 
@@ -19,6 +19,18 @@ function createDiv(){
 
 }
 
+// Esta função pega o texto da tarefa:
+function getText() {
+  console.log(NEWTASK.childNodes.length);
+  if (NEWTASK.childNodes.length == 1){
+    const input = document.createElement("input");
+    input.type = "text";
+    NEWTASK.appendChild(input);
+  }
+}
+
+
+
 function showDelete(taskTarget) {
   const taskDiv = document.getElementById(taskTarget);
   console.log(taskDiv.childNodes.length);
@@ -28,4 +40,7 @@ ROOT.addEventListener("click", function(event){
   console.log(event.target);
   showDelete(event.target.id);
 })
-NEWTASK.addEventListener("click", createDiv);
+NEWTASK.addEventListener("click", function (event) {
+  getText();
+  createDiv("teste");
+});
